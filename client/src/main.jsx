1c3b9117a12b462f74aac4ad1a13b71c9154eebf
Yash panchal <yashpanchal2905@gmail.com>
2025-11-14 15:37:13 +0530

    updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,11 +6,9 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
