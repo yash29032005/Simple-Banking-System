1e5ea430ad093fd48c640e66e0e6e5f746c87fa8
Yash panchal <yashpanchal2905@gmail.com>
2025-11-14 08:54:33 +0530

    changes in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
   database: process.env.SQL_DATABASE,
+  port: process.env.SQL_PORT,
 });
 
 (async () => {
